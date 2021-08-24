d1dbc5b9890e715886eeb991574c9482f091797b
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2021-08-24 14:52:34 +0530

    done some

---
+++ app.js
@@ -0,0 +1,8 @@
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stock-quantity");
+const currentQuantity = document.querySelector("#current-price");
+ 
+const btnTellMe = document.querySelector("#submit-btn");
+
+const outputDiv = document.querySelector("#output-Div");
+
