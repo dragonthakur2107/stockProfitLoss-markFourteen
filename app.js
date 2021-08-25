0d7734c1bd2fb72bb378c93a3277654a5aa02c7a
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2021-08-25 19:36:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function calcProfitAndLoss(initial, quantity, current){
         //  profit
         var profit =(current-initial)*quantity;
         var profitPercent=(profit/initial)*100;
+        document.getElementById("output-Div").style.color= "green";
         showOutput("Good Work! Profit is "+profit+" by "+dotTwoLimiter(profitPercent)+"%");
     }
     else
@@ -20,12 +21,14 @@ function calcProfitAndLoss(initial, quantity, current){
         // loss
         var loss =(initial-current)*quantity;
         var lossPercent=(loss/initial)*100;
+        document.getElementById("output-Div").style.color= "red";
         showOutput("Oh no! loss is "+loss+" by "+dotTwoLimiter(lossPercent)+"%");
     
     }
     else
         {
-            showOutput("Hard luck, no profit ,but hell yeah! no loss either");
+            document.getElementById("output-Div").style.color= "white";
+            showOutput("Hard luck, no profit ,but hell yeah! no loss either.");
         }
 
 }
@@ -40,6 +43,21 @@ btnTellMe.addEventListener("click",function submitHandler(){
           var initial = Number(initialPrice.value);
           var quantity = Number(stocksQuantity.value);
           var current = Number(currentQuantity.value);
+          valueChecker(initial,quantity,current);
 
-          calcProfitAndLoss(initial,quantity,current);
+          
 });
+function valueChecker(initial,quantity,current){
+           if(initial===0 && quantity===0 && current===0 ){
+            document.getElementById("output-Div").style.color= "red";
+               showOutput("Nothing is entered");
+           }
+           else if(initial===0 || quantity===0 || current===0 ){
+            document.getElementById("output-Div").style.color= "red";
+               showOutput("Some fields are missing or is put zero");
+           }
+           else{
+            calcProfitAndLoss(initial,quantity,current);
+           }
+
+}
